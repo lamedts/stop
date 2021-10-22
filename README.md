c7cb329f65edfdf3c1a20c0a3f4a21314b0bd738
Ed <lamedts@gmail.com>
2021-10-22 14:21:33 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# STOP
+
+## TODO:
+
+1. src/react-app-env.d.ts
+2. src/reportWebVitals.ts
+3. src/setupTests.ts
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Router from "./router/Router";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2196f3",
+    },
+    secondary: {
+      main: "#ff5722",
+    },
+  },
+  typography: {
+    fontFamily:
+      'system-ui, -apple-system, "Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
